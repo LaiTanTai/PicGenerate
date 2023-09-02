c55bc6d4659807c4606a443de3b5df967664a528
tyasenpai <tantai204.10@gmail.com>
2023-09-02 13:22:43 +0700

    done

--- src/module/Mainpage/Video/Video.jsx
+++ src/module/Mainpage/Video/Video.jsx
@@ -1,31 +1,43 @@
 import React, { useState } from 'react'
-import styles from './Video.module.scss'
+import './Video.scss'
 function Video() {
-    const picstate = useState({
+    const [picstate,setPicstate] = useState({
         'pic1':true,
         'pic2':false,
         'pic3':false
     })
+    const handleMouseEnter = (number)=>{
+        const setstatestart = {
+            'pic1':false,
+            'pic2':false,
+            'pic3':false
+        }
+        const newstate = {
+            ...setstatestart,
+            [number]:true
+        }
+        setPicstate(newstate)
+    }
   return (
-    <div className={styles.Video}>
+    <div className='Video'>
         <div style={{width:'50%'}}>
-            <h1 className={styles.video_text_h1}>Introducing video generation</h1>
-            <p className={styles.video_text_p}>Provide a prompt and watch Gencraft create unique videos for you in a matter of seconds.</p>
-            <button className={styles.video_text_generate_button}><img src='./img/star.svg' alt='star'/>  Generate Now</button>
+            <h1 className='video_text_h1'>Introducing video generation</h1>
+            <p className='video_text_p'>Provide a prompt and watch Gencraft create unique videos for you in a matter of seconds.</p>
+            <button className='video_text_generate_button'><img src='./img/star.svg' alt='star'/>  Generate Now</button>
         </div>
         <div style={{display:'flex',width:'50%',justifyContent:'center',gap:'30px'}}>
-            <div className={styles.video_hidden}>
-                <video className={styles.video_source} hover width='280px' height='280px' autoPlay loop >
+            <div onMouseEnter={()=>handleMouseEnter('pic1')} className={`video_hidden ${picstate.pic1 ? 'show' : ''}`}>
+                <video className='video_source' width='280px' height='280px' autoPlay loop >
                     <source  src='./video/video1.mp4' type="video/mp4"></source>
                 </video>
             </div>
-            <div className={styles.video_hidden}>
-                <video className={styles.video_source} width='280px' height='280px'  autoPlay loop >
+            <div onMouseEnter={()=>handleMouseEnter('pic2')} className={`video_hidden ${picstate.pic2 ? 'show' : ''}`}>
+                <video className='video_source' width='280px' height='280px'  autoPlay loop >
                     <source  src='./video/video2.mp4' type="video/mp4"></source>
                 </video>
             </div>
-            <div className={styles.video_hidden}>
-                <video className={styles.video_source} width='280px' height='280px' autoPlay loop >
+            <div onMouseEnter={()=>handleMouseEnter('pic3')} className={`video_hidden ${picstate.pic3 ? 'show' : ''}`}>
+                <video className='video_source' width='280px' height='280px' autoPlay loop >
                     <source  src='./video/video3.mp4' type="video/mp4"></source>
                 </video>
             </div>
