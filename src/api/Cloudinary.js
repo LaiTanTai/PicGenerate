41b5fbeafcffcdccd4d8e3ce2764bec80553fd48
tyasenpai <tantai204.10@gmail.com>
2023-09-03 18:30:23 +0700

    done

---
+++ src/api/Cloudinary.js
@@ -0,0 +1,7 @@
+import {v2 as cloudinary} from 'cloudinary';
+          
+cloudinary.config({ 
+  cloud_name: 'dj219rk3m', 
+  api_key: '866498853116259', 
+  api_secret: 'cuAUPbeCDIzF3FXFiMMeS-J01sQ' 
+});
